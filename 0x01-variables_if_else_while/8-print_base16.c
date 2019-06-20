497212fdcba63efd513b2d2c1638504714bacca3
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-20 22:32:07 +0000

    8-print_base16.c Attempt 1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int i = 48;
+for (i = 48 ; i < 58 ; i++)
+{
+putchar(i);
+}
+int j = 97;
+for (j = 97 ; j < 103 ; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
