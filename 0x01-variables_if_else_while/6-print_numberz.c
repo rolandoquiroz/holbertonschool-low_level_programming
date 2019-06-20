2896c1bbd40e4fe79259125dc0b07d19a44dbedb
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-20 21:55:00 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char num_char = 48;
+int i = 0;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar(num_char + i);
+}
+putchar('\n');
+return (0);
+}
