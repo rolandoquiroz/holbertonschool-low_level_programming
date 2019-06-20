e0b28ab72b9952aa885a79173319a1c29c900ad9
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-20 21:15:17 +0000

    3-print_alphabets.c Attempt 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i = 0;
+for (i = 0 ; i < 52 ; i++)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
+}
