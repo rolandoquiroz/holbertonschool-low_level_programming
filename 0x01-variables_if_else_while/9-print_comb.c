cab4fa2fc126e612bb5cacceae62ddacb2299f39
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-21 01:38:03 +0000

    9-print_comb.c Attemp 3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,17 +11,11 @@ int main(void)
 int i;
 for (i = 48 ; i < 58 ; i++)
 {
-if (i < 57)
-{
 putchar(i);
+if (i == 57)
+break;
 putchar(44);
 putchar(32);
-}
-
-if (i == 57)
-{
-putchar(i);
-}
 }
 putchar('\n');
 return (0);
