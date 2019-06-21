e9540581a6ba426368d3714487660e9f88df011f
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-21 07:16:23 +0000

    102-print_comb5.c Attempt 2

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i, j, k, l;
+for (i = 48 ; i < 58 ; i++)
+{
+for (j = 48 ; j < 58 ; j++)
+{
+for (k = 48 ; k < 58 ; k++)
+{
+for (l = 48 ; l < 58 ; l++)
+{
+  if(j < l){
+putchar(i);
+putchar(j);
+putchar(32);
+putchar(k);
+putchar(l);
+ if ((i==57)&&(j==56)&&(k==57)&&(l==57))
+{break;}
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
