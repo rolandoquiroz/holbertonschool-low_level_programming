58ac71edb77eda5e50fe75267a6cae04e74590c9
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-27 15:04:48 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+  int i;
+  for (i = 97; i < 123 ; i++)
+    {
+      _putchar(i);
+    }
+  _putchar(10);
+  return;
+}
