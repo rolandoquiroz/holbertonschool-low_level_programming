440c612f5f0560172288fd777a2a76bb573782a4
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-27 17:13:30 +0000

    2-mul.c attempt 1

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,20 +1,13 @@
 #include "holberton.h"
 /**
- * _isdigit - function that multiplies two integers.
+ * mul - function that multiplies two integers.
  * @a: int a to be multiplied
  * @b: int b to be multiplied
  * Return: 0
  */
 int mul(int a, int b)
 {
-  int ret;
-  if (c > 47 && c < 58)
-    {
-      ret = 1; 
-    }
-  else
-    {
-      ret = 0;  
-    }
-  return (ret);
+int ret;
+ret = a * b;
+return (ret);
 }
