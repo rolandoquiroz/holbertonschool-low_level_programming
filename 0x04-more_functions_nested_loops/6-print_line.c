9467a7d2e580f5d02184f05bfd4fc277f13d54ce
rolandoquiroz <rolandoquiroz@gmail.com>
2019-06-27 19:18:42 +0000

    6-print_line.c Attempt 1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* print_line - function that draws a straight line in the terminal.
+*
+* @n: n is the number of times the character _ should be printed
+*
+* Return: nothing
+*/
+void print_line(int n)
+{
+if (n > 0)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+_putchar(95);
+}
+_putchar(10);
+}
+else
+{
+_putchar(10);
+}
+return;
+}
