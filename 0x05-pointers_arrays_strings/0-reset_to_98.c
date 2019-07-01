215b27431bea8391f09430e802ac458d5d8d0b7b
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-01 23:34:07 +0000

    Betty issues fixed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - to an int as parameter and updates the val it points to to 98
+ *
+ * @*n: int *n
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+int ret;
+if (c > 64 && c < 91)
+{
+ret = 1;
+}
+else
+{
+ret = 0;
+}
+return (ret);
+}
