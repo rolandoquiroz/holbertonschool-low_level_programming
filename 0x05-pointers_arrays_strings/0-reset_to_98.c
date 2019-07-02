70d30174d35beda2a97f4f02ad4d138260b54e6d
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-02 11:13:39 +0000

    0-reset_to_98.c Attempt 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,20 +2,15 @@
 /**
  * reset_to_98 - to an int as parameter and updates the val it points to to 98
  *
- * @*n: int *n
+ * @n: int *n
  *
  * Return: void
  */
 void reset_to_98(int *n)
 {
-int ret;
-if (c > 64 && c < 91)
-{
-ret = 1;
-}
-else
-{
-ret = 0;
-}
-return (ret);
+*n = 80;
+
+
+
+return;
 }
