3e9b836a4d5be1ec1b47f9d56bed9a310af55e00
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-02 15:09:51 +0000

    2-strlen.c Attempt 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen - function that returns the length of a string.
+ *
+ * @s: char *s String to be processed
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+int len;
+len = 0;
+while (s[len] != '\0')
+{
+++len;
+}
+return (len);
+}
