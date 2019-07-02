8384593bdeae83d8a08f6a7692387859b530422f
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-02 16:19:26 +0000

    4-print_rev.c Attempt 1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_rev - Prints a string, in reverse, followed by a new line
+ *
+ * @s: char *s String to be printed
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+int len;
+len = 0;
+while (s[len] != '\0')
+{
+++len;
+}
+while (s[len] >= 0)
+{
+_putchar(s[len]);
+if (len == 0)
+{
+break;
+}
+len--;
+}
+_putchar(10);
+return;
+}
