19e5e681fa9932b05b73577968d0bf79177730f3
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-03 02:35:34 +0000

    9-strcpy.c Attempt 1

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _strcpy - Function that copies a string.
+ *
+ * @dest: *dest buffer pointed to by dest
+ *
+ * @src: *src string pointed to by src
+ *
+ * Return: char. the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len = 0;
+int i = 0;
+while (src[len] != 0)
+{
+len++;
+}
+i = 0;
+while (i <= len)
+{
+dest[i] = src[i];
+if (i == len)
+{
+dest[i + 1] = 0;
+break;
+}
+i++;
+}
+return (dest);
+}
