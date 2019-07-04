4dc160b1c0f408636f37d338eb639457528a78ce
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-04 19:44:58 +0000

    2-strncpy.c Attempt 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,18 +12,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
- i=0;
-while (i < n && src[i] != 0)
-{
-dest[i] = src[i];
-i++;
-}
-i = 0;
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-return (dest);
+  int i = 0;
+
+  for (i = 0; (i < n && src[i] != 0); i++)
+    {
+      dest[i] = src[i];
+    }
+  for (; i < n; i++)
+    {
+      dest[i] = '\0';
+    }
+  return (dest);
 }
