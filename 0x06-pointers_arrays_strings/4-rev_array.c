457caeef86506be28931a4beb7bb1a7709954404
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-04 21:37:26 +0000

    4-rev_array.c Attemp 1

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,23 +10,22 @@
  */
 void reverse_array(int *a, int n)
 {
-  int i;
-  int j;
-  int repo[1000];
-  j = 0;
-  i = n - 1;
-  while (i >= 0)
-    {
-      repo[j] = a[i];
-      j++;
-      i--;
-    }
-i = 0; 
+int i, j, repo[1000];
+i = n - 1;
+j = 0;
 
- while (i < n)
-    {
-      a[i] = repo[i];
-      i++;
-    }
- return;
+while (i >= 0)
+{
+repo[j] = a[i];
+j++;
+i--;
+}
+
+i = 0;
+while (i < n)
+{
+a[i] = repo[i];
+i++;
+}
+return;
 }
