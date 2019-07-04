a03f09134be905c5c839eeae2d04c857659d6083
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-04 18:29:17 +0000

    0-strcat.c Attempt 7

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,23 +10,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-
 int i, j;
-
 i = 0;
-while (src[i] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
-
 j = 0;
-while (dest[j] != '\0')
+while (src[j] != '\0')
 {
-dest[i] = src[j];
-i++;
+dest[i + j] = src[j];
 j++;
 }
-dest[j] = '\0';
-
+dest[i + j] = '\0';
 return (dest);
 }
