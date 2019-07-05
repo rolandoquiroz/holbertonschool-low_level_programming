565ac776f7098f11d84bc5c0e45d6e5a4d84708e
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-05 04:07:35 +0000

    6-cap_string.c Attempt 1

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * string_toupper - Function that capitalizes all words of a string.
+ *
+ * @s: char *s
+ *
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+
+int i, j;
+i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+
+for (j = 0; j < i ; j++)
+{
+if (s[j] == 9 || s[j] == 10 || s[j] == 32 || s[j] == 33 || s[j] == 34 || s[j] == 40 || s[j] == 41 || s[j] == 44 || s[j] == 45 || s[j] == 46 || s[j] == 59 || s[j] == 63 || s[j] == 123 || s[j] == 125)
+{
+if (s[1 + j] >= 97 && s[1 + j] <= 122)
+{
+s[1 + j] = s[1 + j] - 32;
+}
+}
+}
+if (s[0] >= 97 && s[0] <= 122)
+{
+s[0] = s[0] - 32;
+}
+return (s);
+}
