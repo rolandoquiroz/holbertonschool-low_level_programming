b1f36f005cce3c145ae89f87c250be43beca4371
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-08 23:58:32 +0000

    2-strchr.c Attempt 1

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strchr - Function that locates a character in a string.
+ *
+ * @s: char *s
+ *
+ * @c: char c
+ *
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != c && *s != '\0')
+{
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+else
+{
+return (0);
+}
+}
