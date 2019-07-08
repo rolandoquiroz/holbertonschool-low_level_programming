db7c9a69aa129994a56df0d3dbb8d183662283a3
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-08 23:47:31 +0000

    1-memcpy.c Attempt 4

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,8 +12,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-
-while (n--)
-*dest++ = *src++;
+unsigned int i;
+i = 0; 
+while ( i < n )
+{
+dest[i] = src[i];
+i++;
+}
 return dest;
 }
