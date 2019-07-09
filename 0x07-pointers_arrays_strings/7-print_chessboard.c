ff46ec1ba75ee92703b9680e29512120259c4f68
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-09 03:59:29 +0000

    7-print_chessboard.c Attempt 1

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_chessboard - Function that prints the chessboard.
+ *
+ * @a: Array
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+i = 0;
+while (i < 8)
+{
+j = 0;
+while (j < 8)
+{
+if (a[i][j] != 0)
+_putchar(a[i][j]);
+j++;
+}
+_putchar(10);
+i++;
+}
+return;
+}
