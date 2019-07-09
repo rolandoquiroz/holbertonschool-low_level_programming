fd028a38d33cad62fdcdaecc4e5a6a9e722b9534
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-09 01:41:04 +0000

    4-strpbrk.c Attempt 9

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,19 +11,13 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
-i = 0;
-while (s[i] != '\0')
+for (i = 0; s[i] != '\0'; i++)
 {
-j = 0;
-while (accept[j] != '\0')
+for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
-{
 return (s + i);
 }
-j++;
-}
-i++;
 }
 return (s + i);
 }
