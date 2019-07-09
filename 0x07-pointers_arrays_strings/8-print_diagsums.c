5266a8582d625d81e5c17b4d25cdc5bf1720ed6d
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-09 04:42:36 +0000

    8-print_diagsums.c Attempt 1

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Prints the sum of the two diag of a square mtrx of ints.
+ *
+ * @a: int *a
+ *
+ * @size: int size
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+int i, acx, acy;
+acx = 0;
+i = 0;
+while (i < size * size)
+{
+acx = acx + a[i];
+i = i + size + 1;
+}
+acy = 0;
+i = size - 1;
+while (i < (size * size - 1))
+{
+acy = acy + a[i];
+i = i + size - 1;
+}
+printf("%d, %d\n", acx, acy);
+return;
+}
