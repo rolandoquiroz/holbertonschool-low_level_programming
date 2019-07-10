89f4b4ea7ad11e34a14902186ccef94469786850
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-10 18:40:47 +0000

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - Function that prints a string in reverse.
+ *
+ * @s: char *s String to be printed in reverse
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+_print_rev_recursion(s + 1);
+_putchar(*s);
+return;
+}
