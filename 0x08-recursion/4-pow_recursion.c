16dcbdadca52a8748c16ce18f01f06baaf025d1c
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-11 02:12:19 +0000

    4-pow_recursion.c Attempt 1

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - Function that returns the value of x^y.
+ *
+ * @x: int x is the base
+ *
+ * @y: int y is the exponent
+ *
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < -1)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+if (y == 1)
+{
+return (x);
+}
+return (x * _pow_recursion(x, y - 1));
+}
