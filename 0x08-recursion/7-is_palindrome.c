0d75c8bb56f9b4a640046ff8637de004085f5f19
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-12 01:55:31 +0000

    7-palindrome.c Attempt 3

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -14,20 +14,48 @@ return (1 + _strlen_recursion(s + 1));
 }
 return (0);
 }
-
-    
-int is_palindrome(char *s)                             
-{                                                                              
-                                                                              
+/**
+ * _palindrome - Evaluates if a string is a palindrome and or not.
+ *
+ * @s: char *s is the string to be checked
+ *
+ * @left_index: Evalutes string from left
+ *
+ * @right_index: Evaluates string from right
+ *
+ * Return: 1 - Palindrome string  | 0 - Not Palindrome string
+ */
+int _palindrome(char *s, int len)
+{
+if (len <= 1)
+{
+return (1);
+}
+else if (*s != *(s + (len - 1)))
+{
+return (0);
+}
+else
+{
+return (_palindrome(s + 1, (len - 1) - 1));
+}
 }
 /**
  * is_palindrome - Returns 1 if a string is a palindrome and 0 if not.
- *
+y *
  * @s: char *s is the string to be checked
  *
  * Return: 1 - Palindrome string  | 0 - Not Palindrome string
  */
-int is_palindrome(char *s)                                                  
+int is_palindrome(char *s)
 {
-  
+int len = _strlen_recursion(s);
+if (len <= 1)
+{
+return (1);
+}
+else
+{
+return (_palindrome(s, len));
+}
 }
