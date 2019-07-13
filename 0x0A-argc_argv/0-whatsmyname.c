1e4ff86e2387776ac696c01eacaa350136441335
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-13 15:49:54 +0000

    0-whatsmyname.c Attempt 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name, followed by a new line.
+ *
+ * @argc: Number of arguments supplied
+ *
+ * @argv: Pointer to array of arguments supplied
+ *
+ * Return: Int, 0
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
