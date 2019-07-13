8aa027179112008fec6e196af95a8594b1fae17c
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-13 16:15:14 +0000

    1-args.c Attempt 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints the number of arguments passed into it.
+ *
+ * @argc: Number of arguments supplied
+ *
+ * @argv: Pointer to array of arguments supplied
+ *
+ * Return: Int, 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+printf("%d\n", (argc - 1));
+return (0);
+}
