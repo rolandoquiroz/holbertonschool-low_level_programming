d34159d31fa5a74f30ee822990b602a240b940ad
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-17 02:19:48 +0000

    3-alloc_grid.c Attempt 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	int w, h;
 	int **grid;
 
-	if (width < 1 || height < 1)
+	if (width == 0 || height == 0)
 		return (NULL);
 
 	grid = malloc(sizeof(int *) * height);
