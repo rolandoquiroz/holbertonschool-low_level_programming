8683ccedaae6dca6b9558f1a036d9825c512b6ed
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-17 01:15:29 +0000

    2-str_concat.c Attempt 4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,13 +13,13 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int size1 = 0, size2 = 0, size3 = 0, i = 0;
+	int size1 = 0, size2 = 0, size3 = 0, i = 0;
 	char *total;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		S2 = "";
+		s2 = "";
 
 	while (s1[size1] != '\0')
 		size1++;
@@ -31,23 +31,24 @@ char *str_concat(char *s1, char *s2)
 	total = malloc(size3);
 
 	if (total == NULL)
-		total = 0;
+	{
+		return (NULL);
+	}
 	else
 	{
-	while (*s1 != '\0')
-	{
-		total[i] = *s1;
-		s1++;
-		i++;
-	}
-	while (*s2 != '\0')
-	{
-		total[i] = *s2;
-		s2++;
-		i++;
-	}
-	total[i] = '\0';
+		while (*s1 != '\0')
+		{
+			total[i] = *s1;
+			s1++;
+			i++;
+		}
+		while (*s2 != '\0')
+		{
+			total[i] = *s2;
+			s2++;
+			i++;
+		}
+		total[i] = '\0';
 	}
 	return (total);
-	}
 }
