d98367ac1cdd3647499767308e28e74458ba2279
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-18 21:39:08 +0000

    2-calloc.c Attempt 1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _memset - Function that fills memory with a constant byte.
+ *
+ * @s: char *dest
+ *
+ * @b: char *src
+ *
+ * @n: unsigned int n
+ *
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while ((n--) > 0)
+		*ptr++ = b;
+	return (s);
+}
+
+/**
+ * _calloc - Function that allocates memory for an array, using malloc.
+ *
+ * @nmemb: array to be allocated.
+ *
+ * @size: size of the array.
+ *
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *pnt;
+
+pnt = malloc(nmemb * size);
+if (!pnt)
+{
+return (NULL);
+}
+return (_memset(pnt, 0, nmemb * size));
+}
