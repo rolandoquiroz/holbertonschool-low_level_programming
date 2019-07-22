9823bf45aafb89027b61fe26099bf79b2048bacd
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-22 18:46:52 +0000

    1-init_dog.c Attempt 2 : Add d == NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
-	d->age = email;
-	d->owner = age;
-	return (void);
+	d->age = age;
+	d->owner = owner;
+	return;
 }
