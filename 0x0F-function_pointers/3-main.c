ddac72597d9489bb88bc165e3a8d6a6dbdb5a7e1
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-25 02:47:41 +0000

    Commit # 9: 3-main.c Full locally tested

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,18 @@
+
+#include "3-calc.h"
+/**
+ * main - main function.
+ * @argc: count of input parameters
+ * @argv: array of input parameters
+ * Return: 0 success
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+		return (0);
+	}
+	printf("Error\n");
+	return (98);
+}
