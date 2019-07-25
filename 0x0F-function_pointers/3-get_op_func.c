a990dbadef04dda264177af8f75c1da08f828c64
rolandoquiroz <rolandoquiroz@gmail.com>
2019-07-25 03:39:18 +0000

    Commit # 14: 3-get_op_func.c Logic issues solved

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,22 +9,22 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+		{NULL, NULL}};
 
-	while (ops[i].op)
+	i = 0;
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s))
-			i++;
-		else
+		if (strcmp(ops[i].op, == 0))
 			return (ops[i].f);
+		i++;
 	}
 	printf("Error\n");
 	exit(99);
