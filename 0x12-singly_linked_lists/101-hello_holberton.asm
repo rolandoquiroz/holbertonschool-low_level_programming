e765ed2617b1ae9efe34fc5af471537e7c92a296
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-01 17:23:13 +0000

    Commit # 3: 101-hello_holberton.asm added with complete comments

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,9 +5,9 @@
 	section .text
 main:
 	mov	rdi, format 	;set 1st parameter (format)
-	mov	rax, 0
+	mov	rax, 0		;no vectors registers in use
 	call	printf 		;printf(format, )
-	mov	rax, 0
+	mov	rax, 0		;normal exit
 	ret
 format:
 	db "Hello, Holberton", 10, 0 ;C strings need a zero byte at the end
