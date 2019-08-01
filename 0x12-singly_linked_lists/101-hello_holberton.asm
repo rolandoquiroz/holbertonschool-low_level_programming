b503c6fc66ca29f32ae7019ecc4c31ef76d75494
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-01 16:57:11 +0000

    Commit # 2: 101-hello_holberton.asm added with comments

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,13 @@
+
 	global 	main
 	extern 	printf
 
 	section .text
 main:
-	mov	rdi, format
+	mov	rdi, format 	;set 1st parameter (format)
 	mov	rax, 0
-	call	printf
+	call	printf 		;printf(format, )
 	mov	rax, 0
 	ret
 format:
-	db "Hello, Holberton", 10, 0
+	db "Hello, Holberton", 10, 0 ;C strings need a zero byte at the end
