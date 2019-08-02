105cbddaf5dd63e117dc7b594e308078db3b20d5
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-02 00:34:04 +0000

    Commit # 11: 2-add_node.c Betty fixed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
 /**
  * add_node - Function to add a new node at the beginning of list_t
  * @head: Pointer to the pointer to list_t
@@ -12,15 +28,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	int len = 0;
+/*	int len = 0;*/
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	for (; str[len]; len++)
-		;
 	new->str = strdup(str);
-	new->len = len;
+	new->len = _strlen(new->str);
 	new->next = *head;
 	*head = new;
 	return (new);
