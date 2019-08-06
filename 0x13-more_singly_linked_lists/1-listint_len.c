0745e265008f24fbfcd8de7a78baca7682ba2331
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-06 14:58:53 +0000

    Commmit # 4: 1-listint_len.c fixed typeinconsistence

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * listin_len -Function that finds the number of elements in list
+ * listint_len - Function that finds the number of elements in list
  * @h: Pointer to the head of listin_len_t list
  * Return: The number of elements in the list
  */
 
-size_t listin_len(const listin_len_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;/*number of nodes*/
 
