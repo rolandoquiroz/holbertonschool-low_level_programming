cc184159c3c8ca861e2e672455a177e519b1656a
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-06 20:38:07 +0000

    Commmit # 13: 7-get_nodeint.c re-added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,21 +10,15 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tmp;
-	unsigned int;
+	listint_t *tmp = head;
+	unsigned int i = 0;
 
-	if (head != NULL)
+	while (tmp != NULL)
 	{
-
-		tmp = head;
-		i = 0;
-		while (tmp != NULL)
-		{
-			if (i == index)
-				return (tmp);
-			i++;
-			tmp = tmp->next;
-		}
+		if (i == index)
+			return (tmp);
+		i++;
+		tmp = tmp->next;
 	}
-	return (NULL);
+	return (0);
 }
