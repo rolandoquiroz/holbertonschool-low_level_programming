b97429bcdffea10280c8cf4655eb15f46bffcd22
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-09 02:14:17 +0000

    Commit # 4: holberton.h updated again adn 1-print_binary.c added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
 
 /**
- * op_add - Makes the sum of two numbers
- * @arg1: First operand
- * @arg2: Second operand
+ * print_binary - Function that prints the binary representation of a number.
+ * @n: Input number to be converted to binary
  *
- * Return: The sum of the two parameters
+ * Return:  Void
  */
 
-{}
+void print_binary(unsigned long int n)
+
+{
+	char bit;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	print_binary(n >> 1);
+	bit = (n & 1) ? '1' : '0';
+	_putchar(bit);
+}
+
+
