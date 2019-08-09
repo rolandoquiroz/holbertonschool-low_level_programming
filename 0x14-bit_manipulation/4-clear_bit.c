470253b258cdf64f5ddc7d99ff3556b83b92d929
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-09 03:36:21 +0000

    Commit # 7: 4-clear_bit.c added

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * op_add - Makes the sum of two numbers
- * @arg1: First operand
- * @arg2: Second operand
+ * clear_bit - sets bit to 0 at given index
+ * @n: pointer to long uint whose bit we want to set
+ * @index: uint position of bit
  *
- * Return: The sum of the two parameters
+ * Return: 1 on success, or -1 on failure
  */
 
-{}
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
