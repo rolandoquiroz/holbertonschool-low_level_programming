16e30987995b35a7e294ebb6a274e9e5b02060a8
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-09 01:22:24 +0000

    Commit # 2: 0-binary_to_uint.c added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,24 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
- * op_add - Makes the sum of two numbers
- * @arg1: First operand
- * @arg2: Second operand
+ * binary_to_uint - Function that converts a binary number to an unsigned int.
+ * @b: b is pointing to a string of 0 and 1 chars
  *
- * Return: The sum of the two parameters
+ * Return: Converted number, or 0 if b is NULL...
  */
 
-{}
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int ans = 0;
+
+	for (; b && *b; b++)
+	{
+		if (*b < '0' || *b > '1')
+			return (0);
+		ans = (ans << 1) + (*b - '0');
+	}
+	return (ans);
+
+}
