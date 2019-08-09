07746fb88166ed77ee191376e9025e3c541bb7e6
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-09 01:24:36 +0000

    Commit # 3: holberton.h updated

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,7 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
