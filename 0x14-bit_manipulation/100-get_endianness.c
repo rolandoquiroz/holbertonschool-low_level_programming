12c10b0f1ac816f4a0a7bcf62cf5256b209e407c
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-09 05:01:44 +0000

    Commit added

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * op_add - Makes the sum of two numbers
- * @arg1: First operand
- * @arg2: Second operand
+ * get_endianness - function that checks endianness
+ * @void: void
  *
- * Return: The sum of the two parameters
+ * Return: 0 or 1
  */
 
-{}
+int get_endianness(void)
+{
+	int i = 1;
+	char *j = (char *)&i;
+
+	if (*j)
+		return (1);
+	return (0);
+}
