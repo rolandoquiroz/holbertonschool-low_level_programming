4d2218efdfe053dd4b8e464be86ad5a4eadcee1f
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-09 04:11:30 +0000

    Commit # 8: 5-flip_bits.c added

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * op_add - Makes the sum of two numbers
- * @arg1: First operand
- * @arg2: Second operand
+ * flip_bits - returns the number of bits needed to flip from one number
+ * to the another
+ * @n: unsigned long integer type
+ * @m: unsigned long integer type
  *
- * Return: The sum of the two parameters
+ * Return: the number of bits neeed to flip
  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int counter = 0, d = n ^ m;
 
-{}
+	for (; d; d >>= 1)
+		counter += d & 1;
+	return (counter);
+}
