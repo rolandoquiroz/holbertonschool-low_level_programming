1a422d6db9ab1eb6d79218d3640d6d24325e22b3
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-09 02:23:42 +0000

    Commit # 5: 2-get_bit.c added

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * op_add - Makes the sum of two numbers
- * @arg1: First operand
- * @arg2: Second operand
- *
- * Return: The sum of the two parameters
+ * get_bit - Gives the value of a bit at a given index.
+ * @n: Number to evaluate.
+ * @index: Index to look for.
+ * Return: Bit at index on number.
  */
 
-{}
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+		n >>= 1;
+	if (i > ((sizeof(unsigned long int) * 8) - 1))
+		return (-1);
+	return (n & 1);
+}
