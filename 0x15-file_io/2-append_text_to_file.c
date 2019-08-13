b94dfe4a20c12251faa569bf71ea6aa8fda5485f
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-13 17:26:38 +0000

    Commit # 1: Empty file added

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,11 @@
+/**
+ * op_add - Makes the sum of two numbers
+* @arg1: First operand
+* @arg2: Second operand
+ *
+* Return: The sum of the two parameters
+ */
+int op_add(int arg1, int arg2)
+{
+	return (arg1 + arg2);
+}
