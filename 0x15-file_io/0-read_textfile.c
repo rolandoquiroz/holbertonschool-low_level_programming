d8f7132b96451d1e9e183074277d5a98a48903f2
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-13 21:50:45 +0000

    Commit # 4: 0-read_textfile.c added

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,52 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 /**
- * op_add - Makes the sum of two numbers
- * @arg1: First operand
- * @arg2: Second operand
- *
- * Return: The sum of the two parameters
+ * read_textfile - Reads a text file and prints it to stdout.
+ * @filename: Name of the file to be written
+ * @letters: Number of letters to read and print
+ * Return: Number of letters it read and printed
  */
-int op_add(int arg1, int arg2)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	return (arg1 + arg2);
+	int fd, num_to_write, num_to_read;
+	char *buffer = malloc(sizeof(char) * (letters + 1));
+
+	if (buffer == NULL)
+		return (0);
+	if (filename == NULL || letters == 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	num_to_read = read(fd, buffer, letters);
+	if (num_to_read < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	buffer[letters] = '\0';
+	num_to_write = write(STDOUT_FILENO, buffer, num_to_read);
+	if (num_to_write <= 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (num_to_write);
 }
