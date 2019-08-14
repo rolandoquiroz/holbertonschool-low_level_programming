ed5ce8ea9dc710a774448f50c07220a3d3a982b1
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-14 01:56:23 +0000

    Commit # 11: 2-append_text_to_file.c added

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 /**
- * op_add - Makes the sum of two numbers
-* @arg1: First operand
-* @arg2: Second operand
- *
-* Return: The sum of the two parameters
+ * _strlen - returns string length of input string
+ * @s: string to check length of
+ * Return: length of string
  */
-int op_add(int arg1, int arg2)
+int _strlen(char *s)
 {
-	return (arg1 + arg2);
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * append_text_to_file - function to append text at the end of the file
+ * @filename: pointer to the file
+ * @text_content: content that needs to be appended
+ *
+ * Return: 1 on sucess -1 on fail
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len;
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (close(fd) == -1 ? -1 : 1);
+	len = _strlen(text_content);
+	if (write(fd, text_content, len) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (close(fd) == -1 ? -1 : 1);
 }
