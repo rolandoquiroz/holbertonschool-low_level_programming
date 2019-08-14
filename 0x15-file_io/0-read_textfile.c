bae5212812c1bcbd1ab467c4f6d469c5f8d80928
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-14 01:43:59 +0000

    Commit # 8: 0-read_textfile.c readded

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,46 +7,50 @@
 #include <fcntl.h>
 
 /**
- * read_textfile - Reads a text file and prints it to stdout.
- * @filename: Name of the file to be written
- * @letters: Number of letters to read and print
- * Return: Number of letters it read and printed
+ * _strlen - returns string length of input string
+ * @s: string to check length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: the file
+ * @letters: number of letters to print
+ *
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, num_to_write, num_to_read;
-	char *buffer = malloc(sizeof(char) * (letters + 1));
+	int fd;
+	ssize_t letters_to_write;
+	char *buffer;
 
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-	if (filename == NULL || letters == 0)
+	if (read(fd, buffer, letters) == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	letters_to_write = _strlen(buffer);
+	letters_to_write = write(STDOUT_FILENO, buffer, letters_to_write);
+	if (letters_to_write == -1)
 	{
-		close(fd);
 		free(buffer);
 		return (0);
 	}
-	num_to_read = read(fd, buffer, letters);
-	if (num_to_read < 0)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	buffer[letters] = '\0';
-	num_to_write = write(STDOUT_FILENO, buffer, num_to_read);
-	if (num_to_write <= 0)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	close(fd);
 	free(buffer);
-	return (num_to_write);
+	return (close(fd) == -1 ? -1 : letters_to_write);
 }
