f446c74409782966423f0a10fc5b2b4b62348438
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-16 02:38:51 +0000

    Commit # 09: 6-sum_dlistint.c added

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "listso.h"
+/**
+ * sum_dlistint - Makes the sum of two numbers
+ * @head: First operand
+ *
+ * Return: The sum of the two parameters
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *pnt;
+	int s = 0;
+
+	if (head == NULL)
+		return (0);
+	pnt = head;
+	while (pnt)
+	{
+		s = s + pnt->n;
+		pnt = pnt->next;
+	}
+	return (s);
+}
