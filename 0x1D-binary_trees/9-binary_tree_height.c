45c1d09cab4dcc93e4f833a54fd6e279823cd047
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-04 20:48:44 +0000

    [Re-Add] 9-binary_tree_height.c

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -9,15 +9,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
+	size_t height_left = 0, height_right = 0;
 
 	if (tree == NULL || ((tree->left == NULL) && (tree->right == NULL)))
 		return (0);
 
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
+	if (tree->left)
+		height_left = binary_tree_height(tree->left);
+	if (tree->right)
+		height_right = binary_tree_height(tree->right);
 
-	if (height_left > height_right)
+	if (height_left >= height_right)
 		return (height_left + 1);
 	else
 		return (height_right + 1);
