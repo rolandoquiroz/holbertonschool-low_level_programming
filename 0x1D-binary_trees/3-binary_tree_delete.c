c5262a8d3d06c1dec7306410a6f6aefcce30ee03
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-04 16:40:41 +0000

    [Add] Task 0 to 8 source files

---
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Function that deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
