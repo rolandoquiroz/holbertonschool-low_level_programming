c532dd9bac2fe5da88b51453fd89734cc7af754a
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-05 15:10:14 +0000

    [Add] 17-binary_tree_sibling.c source file again

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -16,6 +16,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node == node->parent->right)
 		return (node->parent->left);
-	else
+	if (node == node->parent->left)
 		return (node->parent->right);
+	return (NULL);
 }
