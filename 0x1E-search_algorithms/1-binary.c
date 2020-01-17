e9f1cecd99021373f6f53477048ce1bf2ac9b9fb
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-17 02:09:29 +0000

    1-binary.c functions

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,50 @@
 #include "search_algos.h"
 
 /**
+ * binary_search_aux - Function that searches for a value in a sorted
+ * array of integers using the Binary search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @inferior: Is the number of elements in array
+ * @superior: Is the number of elements in array
+ * @value: Is the value to search for
+ *
+ * Return: The first index where value is located or
+ * if value is not present in array or if array is NULL,
+ * your function must return -1
+ */
+
+int binary_search_aux(in *array, size_t inferior, size_t superior, int value)
+{
+	int i = 0, medium = 0;
+
+	printf("Searching in array: %d", array[inferior]);
+	i = inferior + 1;
+	while (i <= superior)
+	{
+		printf(", %d", array[i]);
+		i++;
+	}
+	printf("\n");
+
+	if (inferior > superior)
+		return (-1);
+	medium = (inferior + superior) / 2;
+	if (value > array[medium])
+	{
+		return (binary_search_aux(array, medium + 1, superior));
+	}
+	else if (value < array[medium])
+	{
+		return (binary_search_aux(array, inferior, medium - 1));
+	}
+	else
+	{
+		return (medium);
+	}
+}
+
+
+/**
  * binary_search - Function that searches for a value in a sorted
  * array of integers using the Binary search algorithm
  * @array: Pointer to the first element of the array to search in
