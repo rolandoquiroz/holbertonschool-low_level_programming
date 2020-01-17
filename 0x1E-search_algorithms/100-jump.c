8d96050a457f3454d537532b57bc2309d4a6c78d
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-17 05:28:47 +0000

    Task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -46,6 +46,7 @@ int jump_search(int *array, size_t size, int value)
 		}
 
 		left += jump;
+
 	}
 
 	if (left >= size || array[left] > value)
@@ -54,6 +55,7 @@ int jump_search(int *array, size_t size, int value)
 	}
 
 	right = min(size - 1, right);
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 	for (i = left; i <= right && array[i] <= value; ++i)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
