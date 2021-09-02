17655465fc252c8ec3fa95b46063afb384f54f2a
rolandoquiroz <rolandoquiroz@gmail.com>
2021-09-01 23:04:01 -0500

    Fix: Update logic

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,69 +1,34 @@
 #include "search_algos.h"
-
+#include <math.h>
 /**
- * min - Searches for min value between a and b
- * @a: Number a
- * @b: Number b
- * Return: min value between a and b.
- */
-
-int min(int a, int b)
-{
-	return (a < b ? a : b);
-}
-
-/**
- * jump_search - Function that searches for a value in a sorted
- * array of integers using the jump search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Is the number of elements in array
- * @value: Is the value to search for
- *
- * Return: The first index where value is located or
- * if value is not present in array or if array is NULL,
- * your function must return -1
- */
-
+* jump_search - Searches for a value in a sorted array of integers using the
+*		Jump search algorithm.
+* @array: Pointer to the first element of the array to search in.
+* @size: Number of elements in array.
+* @value: Value to search for.
+* Return: First index where value is located or -1 if not.
+*/
 int jump_search(int *array, size_t size, int value)
 {
-	int jump = (int) sqrt(size);
-	size_t left = 0;
-	size_t right = 0;
-	size_t i = 0;
+	unsigned int sqroot, i, j;
 
-	if ((!array) || (!size))
+	if (!array || !size || array[0] > value)
 		return (-1);
-
-	while (left < size && array[left] <= value)
+	sqroot = sqrt(size);
+	for (i = 0; i < size; i += sqroot)
 	{
-		right = min(size - 1, left + jump);
-		printf("Value checked array[%lu] = [%d]\n", left, array[left]);
-		if (array[left] <= value && array[right] >= value)
-		{
+		if (array[i] >= value)
 			break;
-		}
-
-		left += jump;
-
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	}
-
-	if (left >= size || array[left] > value)
+	printf("Value found between indexes [%d] and [%d]\n", i - sqroot, i);
+	for (j = i - sqroot; j <= i; j++)
 	{
-		printf("Value found between indexes [%d] and [%d]\n",
-		       (int)left - jump, (int)left);
-		printf("Value checked array[%d] = [%d]\n", (int)left - jump,
-		       array[(int)left - jump]);
-		return (-1);
+		if (j >= size)
+			return (-1);
+		printf("Value checked array[%d] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
 	}
-
-	right = min(size - 1, right);
-	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
-	for (i = left; i <= right && array[i] <= value; ++i)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
-
 	return (-1);
 }
