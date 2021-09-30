c0711d4dc5e404c683ca4c95015312ad370e2654
rolandoquiroz <rolandoquiroz@gmail.com>
2021-09-30 17:00:10 -0500

    [Update] Deletes trash files and does betty to all .c task files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
@@ -6,6 +7,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
 	return (1);
 }
