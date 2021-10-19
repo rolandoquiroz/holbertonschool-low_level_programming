3c6b24817194ba9917468ae12c86a6eedcd04495
rolandoquiroz <rolandoquiroz@gmail.com>
2021-10-18 23:46:22 -0500

    Update files 5 to 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * set_string - Set the value of a pointer to a char.
+ *
+ * @s: char **s
+ *
+ * @to: char *to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
