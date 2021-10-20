519cb087f45d4a6374bc56da1f40b14de76499ab
rolandoquiroz <rolandoquiroz@gmail.com>
2021-10-20 17:09:03 -0500

    Updates indentation for file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,18 +10,18 @@
  */
 int watcher(int n, int l)
 {
-if (l * l == n)
-{
-return (l);
-}
-else if (l * l < n)
-{
-return (watcher(n, l + 1));
-}
-else
-{
-return (-1);
-}
+	if (l * l == n)
+	{
+		return (l);
+	}
+	else if (l * l < n)
+	{
+		return (watcher(n, l + 1));
+	}
+	else
+	{
+		return (-1);
+	}
 }
 /**
  * _sqrt_recursion - Function that returns the natural square root of a number.
@@ -32,12 +32,12 @@ return (-1);
  */
 int _sqrt_recursion(int n)
 {
-if (n == 0 || n == 1)
-{
-return (n);
-}
-else
-{
-return (watcher(n, 1));
-}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (watcher(n, 1));
+	}
 }
