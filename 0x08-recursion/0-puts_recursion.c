53739762479fd5f56ab32928bb09b8aa64691e5e
rolandoquiroz <rolandoquiroz@gmail.com>
2021-10-20 22:26:45 -0500

    Updates indentation for file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-_putchar(10);
-else
-{
-_putchar(*s);
-s++;
-_puts_recursion(s);
-}
-return;
+	if (*s == '\0')
+		_putchar(10);
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
 }
--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -8,12 +8,11 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s)
-{
-return (1 + _strlen_recursion(s + 1));
+	if (!(*s))
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
-return (0);
-}
+
 /**
  * _palindrome - Evaluates if a string is a palindrome and or not.
  *
@@ -25,19 +24,13 @@ return (0);
  */
 int _palindrome(char *s, int l)
 {
-if (l <= 1)
-{
-return (1);
+	if (l <= 1)
+		return (1);
+	if (*s != *(s + (l - 1)))
+		return (0);
+	return (_palindrome(s + 1, (l - 1) - 1));
 }
-else if (*s != *(s + (l - 1)))
-{
-return (0);
-}
-else
-{
-return (_palindrome(s + 1, (l - 1) - 1));
-}
-}
+
 /**
  * is_palindrome - Returns 1 if a string is a palindrome and 0 if not.
 y *
@@ -47,14 +40,5 @@ y *
  */
 int is_palindrome(char *s)
 {
-int l = 0;
-l = _strlen_recursion(s);
-if (l <= 1)
-{
-return (1);
-}
-else
-{
-return (_palindrome(s, l));
-}
+	return (_palindrome(s, _strlen_recursion(s)));
 }
