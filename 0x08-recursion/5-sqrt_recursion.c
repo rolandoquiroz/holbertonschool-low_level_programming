797288f7ddbf4854ebd44137937b943cf10356c1
rolandoquiroz <rolandoquiroz@gmail.com>
2021-10-20 19:18:07 -0500

    Updates indentation for file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- *_sqrt_n_greater_than_one - Function that calculates a natural sqr if n > 1
+ *_sqrt_when_n_greater_than_one - Calculate the natural sqrt for n, if n > 1
  *
- *@n: int n
+ *@m: int m candidate to be the natural square root of a n
  *
- *@l: int l
+ *@n: int n number to calculate its natural square root
  *
- *Return: A natural square root
+ *Return: int natural square root of n
  */
-int _sqrt_n_greater_than_one(int n, int l)
+int _sqrt_when_n_greater_than_one(int m, int n)
 {
-	if (l * l == n)
-		return (l);
-	else if (l * l < n)
-		return (_sqrt_n_greater_than_one(n, l + 1));
+	if (m * m == n)
+		return (m);
+	else if (m * m < n)
+		return (_sqrt_when_n_greater_than_one(m + 1, n));
 	else
 		return (-1);
 }
@@ -22,12 +22,12 @@ int _sqrt_n_greater_than_one(int n, int l)
  *
  * @n: int n number to calculate its natural square root
  *
- * Return: int
+ * Return: int the natural square root of a n.
  */
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
 	else
-		return (_sqrt_n_greater_than_one(n, 1));
+		return (_sqrt_when_n_greater_than_one(1, n));
 }
