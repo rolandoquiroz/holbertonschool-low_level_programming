a958ef2a10218b260e01588cd71f6ba29e364f67
rolandoquiroz <rolandoquiroz@gmail.com>
2022-04-05 23:22:08 -0500

    Add *-main.c files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - Function that converts a binary number to an unsigned int.
+ * binary_to_uint - Function that converts a binary number to an unsigned int
  * @b: b is pointing to a string of 0 and 1 chars
  *
- * Return: Converted number, or 0 if b is NULL...
+ * Return: Converted number
+ *		, or 0 if:
+ *			- there is one or more chars in the string b that is not 0 or 1
+ *			- b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
