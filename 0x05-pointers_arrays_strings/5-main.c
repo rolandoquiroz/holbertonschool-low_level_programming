a3fcdaba2557eda8cc6d06fc6c3018db6c9f2189
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-24 01:04:07 -0500

    Update task

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -9,9 +9,18 @@
 int main(void)
 {
 	char s[10] = "Holberton";
+	char str[7] = "Holbie";
+	char ch[2] = "H";
 
 	printf("%s\n", s);
 	rev_string(s);
 	printf("%s\n", s);
+	printf("%s\n", str);
+	rev_string(str);
+	printf("%s\n", str);
+	printf("%s\n", ch);
+	rev_string(ch);
+	printf("%s\n", ch);
+
 	return (0);
 }
