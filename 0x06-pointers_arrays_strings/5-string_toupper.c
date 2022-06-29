b90779b384ece482bbc8a895594235eef197b37b
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-29 03:19:30 -0500

    Update task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase.
  *
@@ -8,20 +9,15 @@
  */
 char *string_toupper(char *s)
 {
-int len, i;
-len = 0;
-while (s[len] != '\0')
-{
-len++;
-}
-i = 0;
-while (i < len)
-{
-if (s[i] >= 97 && s[i] <= 122)
-{
-s[i] =  s[i] - 32;
-}
-i++;
-}
-return (s);
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+			*(s + i) -= 32;
+		i++;
+	}
+
+	return (s);
 }
