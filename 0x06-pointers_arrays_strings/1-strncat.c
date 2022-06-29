29a6dded2f229e121f78b18f2e4c543e5b784c6c
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-28 23:40:23 -0500

    Update task 0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
+
 /**
- * _strncat - Function that concatenates two strings.
+ * _strncat - Function that concatenates two strings. The _strncat function is
+ * similar to the _strcat function, except that:
+ *  - it will use at most n bytes from src
+ *  - src does not need to be null-terminated if it contains n or more bytes
  * @dest: char *dest
  * @src: char *src
  * @n: int n
  *
- * Return: void
+ * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
