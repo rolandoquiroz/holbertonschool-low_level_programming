bbee24bc0c733ba7a1475353dfd46c2109bb481c
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-28 19:33:38 -0500

    Update task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,11 +9,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
+	int i = _strlen(dest), j;
 
 	j = 0;
 	while ((j < n) && (src[j] != '\0'))
