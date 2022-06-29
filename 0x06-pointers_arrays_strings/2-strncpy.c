411a59ff128129caf3c11f6f1798151ca3abb7e7
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-29 00:00:32 -0500

    Update task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,7 @@
 #include "holberton.h"
 /**
  * _strncpy - Function that copies a string.
- *
  * @dest: char *dest
- *
  * @src: char *src
  *
  * @n: int n
@@ -12,17 +10,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-i = 0;
-while (i < n && src[i] != 0)
-{
-dest[i] = src[i];
-i++;
-}
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-return (dest);
+	int i;
+
+	i = 0;
+	while ((*(src + i) != 0) && (i < n))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+
+	return (dest);
 }
