8144f458a04d9983091e3b4082d116cac1a7cb40
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-28 23:25:09 -0500

    Update task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,10 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i + j] = src[j];
+		*(dest + i + j) = *(src + j);
 		j++;
 	}
-	dest[i + j] = '\0';
+	*(dest + i + j) = '\0';
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	while ((src[j] != '\0') && (j < n))
 	{
-		dest[i + j] = src[j];
+		*(dest + i + j) = *(src + j);
 		j++;
 	}
 
