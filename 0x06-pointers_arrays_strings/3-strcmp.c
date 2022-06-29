36b47676726c2234cbdeacc9cec1d53e1a7c0b3c
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-29 00:20:59 -0500

    Update task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,16 @@
 #include "holberton.h"
+
 /**
  * _strcmp - Function that compares two strings.
- *
  * @s1: char *s1
- *
  * @s2: char *s2
  *
  * Return: int
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s2 && *s1 == *s2)
-{
-++s1;
-++s2;
-}
-return ((*s1) - (*s2));
+	while ((*s1 && *s2) && (*s1 == *s2))
+		++s1, ++s2;
+
+	return (*s1 - *s2);
 }
