e1a88337a706cc712b9a09fb49ffb600afba89f6
rolandoquiroz <rolandoquiroz@gmail.com>
2022-06-29 02:07:06 -0500

    Update task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,31 +1,27 @@
 #include "holberton.h"
+
 /**
  * reverse_array -  reverse an array of integers
- *
  * @a: int *a
- *
  * @n: int n
  *
  * Return: void
  */
 void reverse_array(int *a, int n)
 {
-int i, j, repo[1000];
-i = n - 1;
-j = 0;
+	int *b;
+	int i, left, right;
 
-while (i >= 0)
-{
-repo[j] = a[i];
-j++;
-i--;
-}
+	b = a;
 
-i = 0;
-while (i < n)
-{
-a[i] = repo[i];
-i++;
-}
-return;
+	i = 0;
+	while (i < (n - 1))
+		b++, i++;
+
+	while (i > (n / 2))
+	{
+		left = *a, right = *b;
+		*a = right, *b = left;
+		a++, b--, i--;
+	}
 }
