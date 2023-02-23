3e2943220d691b50bfafd06f2a85cd0b39fbee43
rolandoquiroz <rolandoquiroz@gmail.com>
2023-02-23 14:46:19 -0500

    Update task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,17 +12,16 @@ void times_table(void)
 		{
 			if ((i * j) < 10)
 			{
+				_putchar('0' + (i * j));
 				if ((i == 2 && j == 4) || (i == 3 && j == 3) || (i == 4 && j == 2) ||
 				    (i >= 5 && j == 1))
 				{
-					_putchar('0' + (i * j));
 					if (j == 9)
 						_putchar('\n');
 					_putchar(','), _putchar(' ');
 				}
 				else
 				{
-					_putchar('0' + (i * j));
 					if (j == 9)
 					{
 						_putchar('\n');
