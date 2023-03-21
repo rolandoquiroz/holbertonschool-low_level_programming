662b028a763d5b6af6e40accc82c2733792e85aa
rolandoquiroz <rolandoquiroz@gmail.com>
2023-03-21 17:31:00 -0500

    Update Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - calculates the length of a given string.
+ * @s: string to be measured
+ *
+ * Return: length of the string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
  * str_concat - Function that concatenates two strings.
  * @s1: String s1 given as a first parameter.
  * @s2: String s2 given as a second parameter.
@@ -11,42 +27,29 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size1 = 0, size2 = 0, size3 = 0, i = 0;
-	char *total;
+	unsigned int size1 = 0, size2 = 0, i = 0;
+	char *s1s2 = NULL;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[size1] != '\0')
-		size1++;
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
 
-	while (s2[size2] != '\0')
-		size2++;
+	s1s2 = (char *)malloc((size1 + size2 + 1) * (sizeof(char)));
 
-	size3 = size1 + size2 + 1;
-	total = malloc(size3);
-
-	if (total == NULL)
-	{
+	if (s1s2 == NULL)
 		return (NULL);
-	}
-	else
-	{
-		while (*s1 != '\0')
-		{
-			total[i] = *s1;
-			s1++;
-			i++;
-		}
-		while (*s2 != '\0')
-		{
-			total[i] = *s2;
-			s2++;
-			i++;
-		}
-		total[i] = '\0';
-	}
-	return (total);
+
+	for (i = 0; i < size1; i++)
+		*(s1s2 + i) = *(s1 + i);
+
+	for (i = 0; i < size2; i++)
+		*(s1s2 + (size1 + i)) = *(s2 + i);
+
+	*(s1s2 + (size1 + size2)) = '\0';
+
+	return ((char *)s1s2);
 }
