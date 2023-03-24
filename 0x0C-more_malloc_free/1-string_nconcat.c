bf9e544965797188ad262422072ede115274b42c
rolandoquiroz <rolandoquiroz@gmail.com>
2023-03-24 16:38:55 -0500

    Update task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 
 /**
- * _strlen - Calcualte the lenght of an array
- * @s: The array to be calculated
+ * _strlen - calculate the length of an string
+ * @s: string to be measured
  *
- * Return: On success a pointer.
- * On error, NULL is returned.
+ * Return: length of the string
  */
 
 unsigned int _strlen(char *s)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
+	for (; *(s + i) != '\0'; i++)
+		;
+
 	return (i);
 }
 
@@ -23,9 +22,7 @@ unsigned int _strlen(char *s)
  * string_nconcat - Function that concatenates two strings.
  *
  * @s1: char *s1 is the first string to concatenate.
- *
  * @s2: char *s2 is the second string to concatenate.
- *
  * @n: partial lenght of s2 to be concatenated.
  *
  * Return: char
@@ -33,39 +30,32 @@ unsigned int _strlen(char *s)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k;
-	char *pnt;
-
-	k = 0;
+	unsigned int size1 = 0, i = 0;
+	char *s1s2_n_ = NULL;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+
+	size1 = _strlen(s2);
 
 	if (n >= _strlen(s2))
-	{
-		n =  _strlen(s2);
-	}
-	pnt = malloc(sizeof(char) * (_strlen(s1) + n + 1));
-	if (pnt == NULL)
-	{
+		n = _strlen(s2);
+
+	s1s2_n_ = (char *)malloc((size1 + n + 1) * sizeof(char));
+
+	if (s1s2_n_ == NULL)
 		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		pnt[k] = s1[i];
-		k++;
-	}
-	for (j = 0; j < n; j++)
-	{
-		pnt[k] = s2[j];
-		k++;
-	}
-	pnt[k] = '\0';
-	return (pnt);
+
+	for (i = 0; i < size1; i++)
+		*(s1s2_n_ + i) = *(s1 + i);
+
+	for (i = 0; i < n; i++)
+		*(s1s2_n_ + (size1 + i)) = *(s2 + i);
+
+	*(s1s2_n_ + (size1 + n)) = '\0';
+
+	return ((char *)s1s2_n_);
 }
