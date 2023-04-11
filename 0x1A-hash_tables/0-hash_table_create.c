00094d3b1e4fe39da316890da28e8376f9884f73
rolandoquiroz <rolandoquiroz@gmail.com>
2023-04-11 14:59:16 -0500

    Update tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	new_hash_table = malloc(sizeof(hash_table_t));
+	new_hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 		return (NULL);
 
 	new_hash_table->size = size;
-	new_hash_table->array = malloc(size * sizeof(hash_node_t *));
+	new_hash_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 	if (new_hash_table->array == NULL)
 	{
 		free(new_hash_table);
