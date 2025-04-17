25641c673f825a32771bece518fefd30894ee889
rolandoquiroz <rolandoquiroz@gmail.com>
2025-04-17 16:12:53 -0500

    Add a function that prints 10 times the numbers, from 0 to 14, followed by a new line

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char);
+void more_numbers(void);
+
+#endif /* _MAIN_H_ */
