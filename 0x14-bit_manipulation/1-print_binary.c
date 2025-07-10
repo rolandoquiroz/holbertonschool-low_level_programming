e8c5bbc9e109f1fb9fa0b0734a178cf141fc7bf4
rolandoquiroz <rolandoquiroz@gmail.com>
2025-07-09 20:54:54 -0500

    Update task 1 and 5

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,12 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 1 || n == 0)
+	if (n < 2)
 	{
 		_putchar(n + '0');
 		return;
 	}
 
 	print_binary(n >> 1);
-	_putchar((n & 1) ? '1' : '0');
+	_putchar((n & 1) + '0');
 }
